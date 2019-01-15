d3fde928aa5f06a77987c4f4f6b2cb70fee33252
Zurubabel <zurubabel@zurubabel.com>
2019-01-15 00:29:04 -0200

    Commit da aula 4

--- pytddrpg/personagem.py
+++ pytddrpg/personagem.py
@@ -1,10 +1,15 @@
 class Personagem():
 
-    def __init__(self):
-        self.__vida__ = 0
+    def __init__(self, vida):
+        self.__vida__ = vida
+
+    def atacar(self):
+        return 5
 
     def receber_dano(self, dano):
         self.__vida__ -= dano
+
+    def retornar_vida_restante(self):
         return self.__vida__
 
     def set_vida(self, vida):
--- test/test_personagem.py
+++ test/test_personagem.py
@@ -5,10 +5,18 @@ from pytddrpg.personagem import Personagem
 class TestPersonagem(unittest.TestCase):
 
     def test_PersonagemRecebeDanoERetornaAVidaRestanteCalculada(self):
-        personagem = Personagem()
-        personagem.set_vida(50)
+        personagem = Personagem(50)
+        personagem.receber_dano(10)
 
-        self.assertEqual(personagem.receber_dano(10), 40)
+        self.assertEqual(personagem.retornar_vida_restante(), 40)
+
+    def test_PersonagemCausaDanoAOutroPersonagem(self):
+        personagemAtacante = Personagem(50)
+
+        personagemDefensor = Personagem(50)
+        personagemDefensor.receber_dano(personagemAtacante.atacar())
+
+        self.assertEqual(personagemDefensor.retornar_vida_restante(), 45)
 
 if __name__ == "__main__":
     unittest.main()
